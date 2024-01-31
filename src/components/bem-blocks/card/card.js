9a8d664506c2175a910d55ccca4ec63a712aecbd
Ivan Balakin <ivanbal1821@yandex.ru>
2024-01-31 17:39:20 +0300

    stable version

--- src/components/bem-blocks/card/card.js
+++ src/components/bem-blocks/card/card.js
@@ -1,15 +1,102 @@
-let currency = document.querySelectorAll(".card-price-currency")
-let prices = document.querySelectorAll(".card-price-value")
-let period = document.querySelectorAll(".card-price-period")
-const dollarsCardOne = 30 
-const dollarsCardTwo = 276
-const dollarsCardThree = 420
-const rublesCardOne = Math.round(dollarsCardOne * 89.61)
-const rublesCardTwo =  Math.round(dollarsCardTwo * 89.61)
-const rublesCardThree = Math.round(dollarsCardThree * 89.61)
-const eurosCardOne = Math.round(rublesCardOne / 96.98)
-const eurosCardTwo = Math.round(rublesCardTwo / 96.98)
-const eurosCardThree = Math.round(rublesCardThree / 96.98)
+let currency = document.querySelectorAll(".card-price-currency"),
+    prices = document.querySelectorAll(".card-price-value"),
+    period = document.querySelectorAll(".card-price-period")
+
+const dollarsCardOne = 30,
+    dollarsCardTwo = 276,
+    dollarsCardThree = 420,
+    rublesCardOne = Math.round(dollarsCardOne * 89.61),
+    rublesCardTwo =  Math.round(dollarsCardTwo * 89.61),
+    rublesCardThree = Math.round(dollarsCardThree * 89.61),
+    eurosCardOne = Math.round(rublesCardOne / 96.98),
+    eurosCardTwo = Math.round(rublesCardTwo / 96.98),
+    eurosCardThree = Math.round(rublesCardThree / 96.98)
+
+const rubles = (index) => {
+    switch (index) {
+        case 0:
+            return rublesCardOne
+        case 1:
+            return rublesCardTwo
+        case 2:
+            return rublesCardThree
+    }
+}
+
+const euros = (index) => {
+    switch (index) {
+        case 0:
+            return eurosCardOne
+        case 1:
+            return eurosCardTwo
+        case 2:
+            return eurosCardThree
+    }
+}
+
+const dollars = (index) => {
+    switch (index) {
+        case 0:
+            return dollarsCardOne
+        case 1:
+            return dollarsCardTwo
+        case 2:
+            return dollarsCardThree
+    }
+}
+
+const setPrice = (period, price, func, ind) => {
+    if(period === '/Months') {
+        price.textContent = String(func(ind))
+    } else {
+        price.textContent = String(Math.round(func(ind) / 30))
+    }
+}
+
+const changePrices = () => {
+    currency.forEach((element, index) => {
+        let parent = element.parentNode
+        let price = parent.querySelector('.card-price-value')
+        let period = parent.querySelector('.card-price-period').textContent
+
+        if (element.textContent === "$") {
+            element.textContent = "₽"
+            setPrice(period, price, rubles, index)
+        } else if (element.textContent === "₽") {
+            element.textContent = "€"
+            setPrice(period, price, euros, index)
+        } else {
+            element.textContent = "$"
+            setPrice(period, price, dollars, index)
+        }
+    })
+}
+
+const monthOrDay = (elem, priceEl, func, ind) => {
+    if (elem.textContent === '/Months') {
+        elem.textContent = '/Day'
+        priceEl.textContent = String(Math.round(func(ind) / 30))
+    } else {
+        elem.textContent = '/Months'
+        priceEl.textContent = String(func(ind))
+    }
+}
+
+const changePeriod = () => {
+    period.forEach((element, index) => {
+        let parent = element.parentNode
+        let price = parent.querySelector('.card-price-value')
+        let currency = parent.querySelector('.card-price-currency')
+
+        if (currency.textContent === "$") {
+            monthOrDay(element, price, dollars, index)
+        } else if (currency.textContent === "₽") {
+            monthOrDay(element, price, rubles, index)
+        } else {
+            monthOrDay(element, price, euros, index)
+        }
+    })
+}
 
 currency.forEach(element => {
     element.addEventListener('click', () => {
@@ -28,101 +115,3 @@ period.forEach(element => {
         changePeriod()
     })
 });
-
-const changePrices = () => {
-    currency.forEach((element, index) => {
-        let parent = element.parentNode
-        let price = parent.querySelector('.card-price-value')
-        if (element.textContent === "$") {
-            element.textContent = "₽"
-            if (index === 0) {
-                price.textContent = rublesCardOne
-            } else if (index === 1) {
-                price.textContent = rublesCardTwo
-            } else {
-                price.textContent = rublesCardThree
-            }
-        } else if (element.textContent === "₽") {
-            element.textContent = "€"
-            if (index === 0) {
-                price.textContent = eurosCardOne
-            } else if (index === 1) {
-                price.textContent = eurosCardTwo
-            } else {
-                price.textContent = eurosCardThree
-            }
-        } else {
-            element.textContent = "$"
-            if (index === 0) {
-                price.textContent = dollarsCardOne
-            } else if (index === 1) {
-                price.textContent = dollarsCardTwo
-            } else {
-                price.textContent = dollarsCardThree
-            }
-        }
-    })
-}
-
-const changePeriod = () => {
-    period.forEach((element, index) => {
-        let parent = element.parentNode
-        let price = parent.querySelector('.card-price-value')
-        let currency = parent.querySelector('.card-price-currency')
-        if (element.textContent === '/Months') {
-            element.textContent = '/Day'
-            if (index === 0) {
-                if (currency.textContent === "$") {
-                    price.textContent = Math.round(dollarsCardOne / 30)
-                } else if (currency.textContent === "₽") {
-                    price.textContent = Math.round(rublesCardOne / 30)
-                } else {
-                    price.textContent = Math.round(eurosCardOne / 30)
-                }
-            } else if (index === 1) {
-                if (currency.textContent === "$") {
-                    price.textContent = Math.round(dollarsCardTwo / 30)
-                } else if (currency.textContent === "₽") {
-                    price.textContent = Math.round(rublesCardTwo / 30)
-                } else {
-                    price.textContent = Math.round(eurosCardTwo / 30)
-                }
-            } else {
-                if (currency.textContent === "$") {
-                    price.textContent = Math.round(dollarsCardThree / 30)
-                } else if (currency.textContent === "₽") {
-                    price.textContent = Math.round(rublesCardThree / 30)
-                } else {
-                    price.textContent = Math.round(eurosCardThree / 30)
-                }
-            }
-        } else {
-            element.textContent = '/Months'
-            if (index === 0) {
-                if (currency.textContent === "$") {
-                    price.textContent = dollarsCardOne
-                } else if (currency.textContent === "₽") {
-                    price.textContent = rublesCardOne
-                } else {
-                    price.textContent = eurosCardOne
-                }
-            } else if (index === 1) {
-                if (currency.textContent === "$") {
-                    price.textContent = dollarsCardTwo
-                } else if (currency.textContent === "₽") {
-                    price.textContent = rublesCardTwo
-                } else {
-                    price.textContent = eurosCardTwo
-                }
-            } else {
-                if (currency.textContent === "$") {
-                    price.textContent = dollarsCardThree
-                } else if (currency.textContent === "₽") {
-                    price.textContent = rublesCardThree
-                } else {
-                    price.textContent = eurosCardThree
-                }
-            }
-        }
-    })
-}
--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,11 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-   document.querySelector('.card-2').style.opacity = 1;
-
-   setTimeout(() => {
+   if (window.innerWidth >= 1024) {
+      document.querySelector('.card-2').style.opacity = 1;
+   
+      setTimeout(() => {
+         document.querySelector('.card-1').style.opacity = 1;
+      }, 500);
+   
+      setTimeout(() => {
+         document.querySelector('.card-3').style.opacity = 1;
+      }, 1000);
+   } else {
       document.querySelector('.card-1').style.opacity = 1;
-   }, 500);
-
-   setTimeout(() => {
-      document.querySelector('.card-3').style.opacity = 1;
-   }, 1000);
+   
+      setTimeout(() => {
+         document.querySelector('.card-2').style.opacity = 1;
+      }, 500);
+   
+      setTimeout(() => {
+         document.querySelector('.card-3').style.opacity = 1;
+      }, 1000);
+   }
 })
